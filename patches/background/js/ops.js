d09ce0c3e85020e2f6129e8c2339477f62175d11
anticore <slimedev@gmail.com>
2024-02-23 07:41:57 +0000

    Updates

--- patches/background/js/ops.js
+++ patches/background/js/ops.js
@@ -2869,6 +2869,124 @@ CABLES.OPS["f1c76976-4c8f-43a5-a9c7-6e4c7d21c749"]={f:Ops.Math.AddUp,objName:"Op
 
 
 
+
+// **************************************************************
+// 
+// Ops.Ui.VizGraph
+// 
+// **************************************************************
+
+Ops.Ui.VizGraph = function()
+{
+CABLES.Op.apply(this,arguments);
+const op=this;
+const attachments=op.attachments={};
+const
+    inNum1 = op.inFloat("Number 1"),
+    inNum2 = op.inFloat("Number 2"),
+    inNum3 = op.inFloat("Number 3"),
+    inNum4 = op.inFloat("Number 4"),
+    inNum5 = op.inFloat("Number 5"),
+    inNum6 = op.inFloat("Number 6"),
+    inNum7 = op.inFloat("Number 7"),
+    inNum8 = op.inFloat("Number 8"),
+    inReset = op.inTriggerButton("Reset");
+
+op.setUiAttrib({ "height": 150, "resizable": true, "vizLayerMaxZoom": 2500 });
+
+let buff = [];
+
+let max = -Number.MAX_VALUE;
+let min = Number.MAX_VALUE;
+
+inNum1.onLinkChanged =
+    inNum2.onLinkChanged =
+    inNum3.onLinkChanged =
+    inNum4.onLinkChanged =
+    inNum5.onLinkChanged =
+    inNum6.onLinkChanged =
+    inNum7.onLinkChanged =
+    inNum8.onLinkChanged =
+    inReset.onTriggered = () =>
+    {
+        max = -Number.MAX_VALUE;
+        min = Number.MAX_VALUE;
+        buff = [];
+    };
+
+op.renderVizLayer = (ctx, layer) =>
+{
+    const perf = CABLES.UI.uiProfiler.start("previewlayer graph");
+
+    const colors = [
+        "#00ffff",
+        "#ffff00",
+        "#ff00ff",
+        "#0000ff",
+        "#00ff00",
+        "#ff0000",
+        "#ffffff",
+        "#888888",
+    ];
+
+    ctx.fillStyle = "#222";
+    ctx.fillRect(layer.x, layer.y, layer.width, layer.height);
+
+    for (let p = 0; p < op.portsIn.length; p++)
+    {
+        if (!op.portsIn[p].isLinked()) continue;
+        const newVal = op.portsIn[p].get();
+
+        max = Math.max(op.portsIn[p].get(), max);
+        min = Math.min(op.portsIn[p].get(), min);
+
+        if (!buff[p]) buff[p] = [];
+        buff[p].push(newVal);
+        if (buff[p].length > 60) buff[p].shift();
+
+        const texSlot = 5;
+        const mulX = layer.width / 60;
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#555555";
+
+        ctx.beginPath();
+        ctx.moveTo(layer.x, CABLES.map(0, min, max, layer.height, 0) + layer.y);
+        ctx.lineTo(layer.x + layer.width, CABLES.map(0, min, max, layer.height, 0) + layer.y);
+        ctx.stroke();
+
+        ctx.strokeStyle = colors[p];
+
+        ctx.beginPath();
+
+        for (let i = 0; i < buff[p].length; i++)
+        {
+            let y = buff[p][i];
+
+            y = CABLES.map(y, min, max, layer.height, 0);
+            y += layer.y;
+            if (i === 0)ctx.moveTo(layer.x, y);
+            else ctx.lineTo(layer.x + i * mulX, y);
+        }
+
+        ctx.stroke();
+    }
+
+    ctx.fillStyle = "#888";
+    ctx.fillText("max:" + Math.round(max * 100) / 100, layer.x + 10, layer.y + layer.height - 10);
+    ctx.fillText("min:" + Math.round(min * 100) / 100, layer.x + 10, layer.y + layer.height - 30);
+
+    perf.finish();
+};
+
+
+};
+
+Ops.Ui.VizGraph.prototype = new CABLES.Op();
+CABLES.OPS["13c54eb4-60ef-4b9c-8425-d52a431f5c87"]={f:Ops.Ui.VizGraph,objName:"Ops.Ui.VizGraph"};
+
+
+
 window.addEventListener('load', function(event) {
 CABLES.jsLoaded=new Event('CABLES.jsLoaded');
 document.dispatchEvent(CABLES.jsLoaded);
